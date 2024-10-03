d9be84312eed7bb018bdbeb2b11713b121cac509
rachelpepe <rachelmpepe@gmail.com>
2024-10-03 10:04:32 -0700

    adding win game and toggle functions

--- lightsOut.js
+++ lightsOut.js
@@ -11,3 +11,45 @@ const currentGame = {
     "startTime": new Date()
 };
 
+
+
+// GAME LOGIC - allLightsOut() and toggle() functions
+
+// returns true id all lights in the game grid are off, false otherwise
+function allLightsOut(game) {
+    for (let i = 0; i < game.lights.length; i++) {
+        // checks all lights, if even one is on, returns false
+        if (game.lights[i]) {
+            return false;
+        }
+    }
+
+    // all lights checked and none are on
+    return true;
+}
+
+// Toggles the light at (row, column) and each perpendicular light
+function toggle(game, row, column) {
+    const locations = [
+        [row, column], [row - 1, column], [row + 1, column], 
+        [row, column - 1], [row, column + 1]
+        ];
+        for (let location of locations) {
+            row = location[0];
+            column = location[1];
+            if (row >= 0 && row < game.rowCount && 
+                column >= 0 && column < game.columnCount) {
+                    // compute array index
+                    const index = row * game.columnCount + column;
+            
+
+            // toggle the light
+            game.lights[index] = !game.lights[index];
+        }
+    }
+}
+
+
+
+// GAME INPUT AND UI FUNCTIONS
+
