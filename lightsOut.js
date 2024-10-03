e5d2d9a7ab36e78e01b484a9f8a284cbda0208b3
Rachel Pepe <rachelmpepe@gmail.com>
2024-10-03 08:43:18 -0700

    first commit - basic html and game object declaration

---
+++ lightsOut.js
@@ -0,0 +1,13 @@
+
+// game object declaration
+const currentGame = {
+    "rowCount": 3,
+    "columnCount": 3,
+    "lights": [
+        true, true, true,
+        true, true, true,
+        true, true, true
+    ],
+    "startTime": new Date()
+};
+
